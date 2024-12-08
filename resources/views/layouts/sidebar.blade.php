38cba47fa53992dd2286e05c5f9a9c17d28c60e9
Rafiul Karim <rafiulkarim51@gmail.com>
2024-12-08 22:59:00 +0600

    my laraval boilar plate initialize

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,61 @@
+<div class="sidebar" data-background-color="dark">
+    <div class="sidebar-logo">
+        <!-- Logo Header -->
+        <div class="logo-header" data-background-color="dark">
+            <a href="index.html" class="logo">
+                <img
+                    src="{{ asset('assets/img/kaiadmin/logo_light.svg') }}"
+                    alt="navbar brand"
+                    class="navbar-brand"
+                    height="20"
+                />
+            </a>
+            <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                    <i class="gg-menu-right"></i>
+                </button>
+                <button class="btn btn-toggle sidenav-toggler">
+                    <i class="gg-menu-left"></i>
+                </button>
+            </div>
+            <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+            </button>
+        </div>
+        <!-- End Logo Header -->
+    </div>
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-secondary">
+                <li class="nav-item active">
+                    <a
+                        data-bs-toggle="collapse"
+                        href="#dashboard"
+                        class="collapsed"
+                        aria-expanded="false"
+                    >
+                        <i class="fas fa-home"></i>
+                        <p>Dashboard</p>
+{{--                        <span class="caret"></span>--}}
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#base">
+                        <i class="fas fa-layer-group"></i>
+                        <p>Base</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="base">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="components/avatars.html">
+                                    <span class="sub-item">Avatars</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
